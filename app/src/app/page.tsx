40cea0fcd1228714b1793554edfaa294179b11bc
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2024-08-10 18:05:00 +1000

    Add confidence value to prediction result

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,9 +3,10 @@
 import { useState } from "react";
 
 export default function Home() {
-  const url = "https://card-classifier.kevinc.xyz/predict/";
+  const url = "http://127.0.0.1:8000/predict/";
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [prediction, setPrediction] = useState<string | null>(null);
+  const [probability, setProbability] = useState<string | null>(null);
 
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
@@ -35,24 +36,29 @@ export default function Home() {
 
       const data = await response.json();
       setPrediction(data.prediction);
+      setProbability(data.probability);
     } catch (error) {
       console.error("Error uploading image:", error);
-      setPrediction("Error occurred while processing the image.");
+      alert(`Error occurred while processing the image.\n ${error}`);
     }
   };
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen">
-      <div className="text-5xl mb-8">Card Classifier App</div>
-      <input type="file" onChange={handleFileChange} className="mb-4" />
+      <div className="text-3xl mb-8">Card Classifier App</div>
+      <div>Upload a picture of a playing card:</div>
+      <input type="file" onChange={handleFileChange} className="border mb-4" />
       <button
         onClick={handleUpload}
         className="px-4 py-2 bg-blue-500 text-white rounded"
       >
-        Upload
+        Submit
       </button>
       {prediction && (
-        <div className="mt-4 text-xl">Prediction: {prediction}</div>
+        <>
+          <div className="mt-4 text-xl">Prediction: {prediction}</div>
+          <div className="mt-4 text-xl">Confidence: {probability}</div>
+        </>
       )}
     </div>
   );
