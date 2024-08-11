7873e99f6000ee5f7d2837acea0764e2c6dde275
Kevin Chen <47770053+kevinwchen@users.noreply.github.com>
2024-08-11 19:13:48 +1000

    Add minor comments

--- api/main.py
+++ api/main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 # List of allowed origins
 origins = [
-    "http://localhost:3000",
-    "https://playing-card-classifier.vercel.app/",
+    "http://localhost:3000",  # for local testing
+    "https://playing-card-classifier.vercel.app/",  # for prod
 ]
 
 app.add_middleware(
